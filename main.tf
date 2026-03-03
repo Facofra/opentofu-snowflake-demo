129115c55a8a22bdd4cb1cc5181c32b70e113760
Facofra <facundo_olleros@hotmail.com>
2026-03-03 08:48:02 -0300

    github secret

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 # A very simple configuration that doesn't need external providers
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo 'Hello from OpenTofu! Environment: ${var.environment}'"
+    command = "echo 'Hello from OpenTofu! Environment: ${var.environment}. The secret value is: ${nonsensitive(var.my_github_secret)}'"
   }
 }
 
 variable "environment" {
   type    = string
   default = "dev"
+}
+
+variable "my_github_secret" {
+  type      = string
+  sensitive = true # This hides the value from OpenTofu logs
 }
