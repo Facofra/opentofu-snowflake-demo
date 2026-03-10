3167303907e297c5d1ce2d472909dc59e5576af5
Facofra <facundo_olleros@hotmail.com>
2026-03-10 16:46:21 -0300

    aws s3 state

--- snowflake/main.tf
+++ snowflake/main.tf
@@ -1,9 +1,22 @@
-# A very simple configuration that doesn't need external providers
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo 'Hello from OpenTofu! Environment: ${var.environment}. The secret value is: ${nonsensitive(var.my_github_secret)}'"
+terraform {
+  backend "s3" {
+    # configure these values for your environment
+    bucket = "opentofu-state-demo-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile=true
   }
 }
+
+
+# A very simple configuration that doesn't need external providers
+resource "null_resource" "example2" {
+  provisioner "local-exec" {
+    command = "echo 'Hello OpenTofu! Environment: ${var.environment}. The secret value is: ${nonsensitive(var.my_github_secret)}'"
+  }
+}
+
+
 
 variable "environment" {
   type    = string
@@ -13,4 +26,6 @@ variable "environment" {
 variable "my_github_secret" {
   type      = string
   sensitive = true # This hides the value from OpenTofu logs
+  default = "secret_value"
+
 }
