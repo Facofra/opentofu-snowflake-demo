fd4dd369c84f442d723dfca9f1d5cde0d31f9dbd
Facofra <facundo_olleros@hotmail.com>
2026-03-30 11:35:16 -0300

    feature/2

--- snowflaketest/main.tf
+++ snowflaketest/main.tf
@@ -2,7 +2,7 @@
 # A very simple configuration that doesn't need external providers
 resource "null_resource" "example2" {
   provisioner "local-exec" {
-    command = "echo 'Hello OpenTofu! Environment: ${var.environment}. The secret value is:'"
+    command = "echo 'Hello OpenTofu! Environment:. The secret value is: nothing'"
   }
 }
 
