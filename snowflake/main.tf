5415a35bc17eba73e89f9fdf40b2f410ed2a9405
Facofra <facundo_olleros@hotmail.com>
2026-05-20 11:11:19 -0300

    env bloque

--- snowflake/main.tf
+++ snowflake/main.tf
@@ -1,7 +1,6 @@
 
 # A very simple configuration that doesn't need external providers
 resource "null_resource" "example" {
-  count = 0
   provisioner "local-exec" {
     command = "echo 'Hello OpenTofu! Environment'"
   }
