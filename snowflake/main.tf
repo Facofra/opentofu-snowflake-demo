62a852458ba1419aec5aa8c298dac7f68db9c32e
Facofra <facundo_olleros@hotmail.com>
2026-05-20 10:09:23 -0300

    count 0

--- snowflake/main.tf
+++ snowflake/main.tf
@@ -1,6 +1,7 @@
 
 # A very simple configuration that doesn't need external providers
 resource "null_resource" "example" {
+  count = 0
   provisioner "local-exec" {
     command = "echo 'Hello OpenTofu! Environment'"
   }
